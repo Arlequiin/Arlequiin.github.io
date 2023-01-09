980e4a67936bd0c78361757810cd6dc8604a6dca
αρλ <78297845+Arlequiin@users.noreply.github.com>
2023-01-09 03:29:04 +0000

    fix upstream

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/public"), (_, res, next) => {
+app.use(express.static(__dirname + "/resurrection"), (_, res, next) => {
   res.status(404)
   res.sendFile(__dirname + "/404.html")
 });
